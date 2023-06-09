30de2926371297fd7a09cbf6d35812b067619cd8
Elias Meireles <eliasmflilico@gmail.com>
2023-06-09 10:10:49 -0300

    Add separeted compiler option

---
+++ java-included.build.settings.gradle.kts
@@ -0,0 +1,3 @@
+includeBuild("spring-boot-included-builds/build-configuration")
+includeBuild("spring-boot-included-builds/source/source-java")
+includeBuild("spring-boot-included-builds/openapi/openapi-java")
