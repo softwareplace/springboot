7adcd693d6250edc332fe66195d47a84fd601720
Elias Meireles <eliasmflilico@gmail.com>
2023-08-29 08:32:32 -0300

    Fixed gradle.properties path

--- libs.settings.gradle.kts
+++ libs.settings.gradle.kts
@@ -1,7 +1,7 @@
 import java.util.Properties
 
 val properties = Properties()
-val inputStream = rootDir.resolve("build-configuration/gradle.properties").inputStream()
+val inputStream = rootDir.resolve("spring-boot-builder-plugin/build-configuration/gradle.properties").inputStream()
 properties.load(inputStream)
 
 properties.forEach { (key, value) ->
