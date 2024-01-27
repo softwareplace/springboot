52a2f8605d5ad235779c9257509ee046e2d02d00
Elias Meireles <eliasmflilico@gmail.com>
2024-01-26 21:51:18 -0300

    Fixed deploy

--- example/java-example/build.gradle.kts
+++ example/java-example/build.gradle.kts
@@ -1,9 +1,3 @@
-import com.github.softwareplace.springboot.plugin.javabuildsource.jsonLogger
-import com.github.softwareplace.springboot.plugin.javabuildsource.lombok
-import com.github.softwareplace.springboot.plugin.javabuildsource.springWebFlux
-import com.github.softwareplace.springboot.plugin.javabuildsource.test
-import com.github.softwareplace.springboot.plugin.javaopenapi.OpenApiSettings
-import com.github.softwareplace.springboot.plugin.javaopenapi.openApiSettings
 
 
 plugins {
@@ -14,13 +8,13 @@ plugins {
 group = "com.java.example"
 version = "1.0.0"
 
-openApiSettings(OpenApiSettings(additionalModelTypeAnnotations = listOf("@lombok.Data", "@lombok.Builder")))
+//openApiSettings(OpenApiSettings(additionalModelTypeAnnotations = listOf("@lombok.Data", "@lombok.Builder")))
 
 dependencies {
-    implementation(project(":security"))
-    springWebFlux()
-    jsonLogger()
-    lombok()
-    test()
+//    implementation(project(":security"))
+//    springWebFlux()
+//    jsonLogger()
+//    lombok()
+//    test()
 }
 
