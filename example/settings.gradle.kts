43938663e4190b15a90e190e3261b2056ff50998
Elias Meireles <eliasmflilico@gmail.com>
2024-01-30 21:46:17 -0300

    Build script class path configuration

--- example/settings.gradle.kts
+++ example/settings.gradle.kts
@@ -1,20 +1,26 @@
 rootProject.name = "example"
 
+pluginManagement {
+    repositories {
+        mavenCentral()
+        mavenLocal()
+        gradlePluginPortal()
+        maven("https://jitpack.io")
+        maven("https://repo.spring.io/milestone")
+    }
+}
+
+buildscript {
+    dependencies {
+        classpath("com.github.softwareplace.springboot:plugins:0.0.017-SNAPSHOT")
+    }
+}
+
 include(":security")
 include(":java-example")
 include(":kotlin-example")
 include(":webflux-example")
 
 project(":security").projectDir = file("shared-modules/security")
-
-
-pluginManagement {
-    repositories {
-        mavenLocal()
-        mavenCentral()
-        gradlePluginPortal()
-        maven(url = "https://jitpack.io")
-    }
-}
 
 
