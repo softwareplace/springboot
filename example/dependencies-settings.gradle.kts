44cfd53f85c23f3f3754588b71b6df637919c960
Elias Meireles <eliasmflilico@gmail.com>
2024-02-12 18:36:43 -0300

    Removed unnecessary gradle setting.

--- example/dependencies-settings.gradle.kts
+++ example/dependencies-settings.gradle.kts
@@ -1,22 +1,9 @@
-import java.util.*
-
 include(":security")
 include(":java-example")
 include(":kotlin-example")
 include(":webflux-example")
 
 project(":security").projectDir = file("shared-modules/security")
-
-apply("included-build-settings.gradle.kts")
-
-val properties = Properties()
-val inputStream =
-    rootDir.resolve("../gradle.properties").inputStream()
-properties.load(inputStream)
-
-properties.forEach { (key, value) ->
-    System.setProperty(key.toString(), value.toString())
-}
 
 buildscript {
     repositories {
