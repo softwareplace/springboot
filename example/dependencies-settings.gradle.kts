337bcd20b28d3edfb5541fa98e04596ac2e0ae4e
Elias Meireles <eliasmflilico@gmail.com>
2024-02-12 18:18:36 -0300

    Workflow test validation fix

--- example/dependencies-settings.gradle.kts
+++ example/dependencies-settings.gradle.kts
@@ -9,6 +9,14 @@ project(":security").projectDir = file("shared-modules/security")
 
 apply("included-build-settings.gradle.kts")
 
+val properties = Properties()
+val inputStream =
+    rootDir.resolve("../gradle.properties").inputStream()
+properties.load(inputStream)
+
+properties.forEach { (key, value) ->
+    System.setProperty(key.toString(), value.toString())
+}
 
 buildscript {
     repositories {
